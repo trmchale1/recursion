0ea87504e19e6629401ba6913e99bcde93862c25
Tim <trmchale@gmail.com>
2014-11-03 16:11:59 -0500

    fixed productOfOdds

--- productOfOdds.cpp
+++ productOfOdds.cpp
@@ -5,6 +5,9 @@ int productOfOdds(int x[], int y){
 	int isOdd;
 	if (y == 1 && x[0] % 2 == 1){return x[0];}
 	else if (y==1 && x[0] % 2 != 1){return 1;}
+	else if(x[0] % 2 != 1){
+		return  productOfOdds(x+1,y-1);
+	}
 	else{	
 		isOdd = x[0] * productOfOdds(x+1,y-1); 
 	
@@ -13,7 +16,7 @@ int productOfOdds(int x[], int y){
 }
 int main(){
 	int x[] = {1,2,3,4,5,6,7};
-	int y = 6;
+	int y = 7;
 	int product = 0;
 	product = productOfOdds(x,y);
 	cout << product << endl;
